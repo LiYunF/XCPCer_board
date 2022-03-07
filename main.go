408e83a61567d2d38f88570d5d0ad54b26991943
Warner <53263520+FengZhg@users.noreply.github.com>
2022-03-07 21:15:09 +0800

    Update main.go

--- main.go
+++ main.go
@@ -10,15 +10,5 @@ import (
 
 // 主入口函数
 func main() {
-	//fmt.Println(spider.GetNCContestPassAmount(context.Background(), model.TestNowCoderIdWLM))
-	//fmt.Println(spider.GetNCContestRating(context.Background(), model.TestNowCoderIdWLM))
-	checkCodeforcesReturn, err := spider.GetCFContestPassAmount(context.Background(), model.TestCodeForcesIdTeacherDu)
-	if err != nil {
-		fmt.Println("no way")
-	}
-	fmt.Println(checkCodeforcesReturn)
-	for i, e := range checkCodeforcesReturn {
-		fmt.Println(i, *e)
-	}
 
 }
