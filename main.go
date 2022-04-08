696a21df078db886291e06f338bac09101fb3512
Warner <945126479@qq.com>
2022-04-08 17:26:04 +0800

    更新example

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"XCPCer_board/spider/example"
+	"fmt"
 	_ "github.com/FengZhg/go_tools/gin_logrus"
 )
 
 // 主入口函数
 func main() {
-
+	fmt.Println(example.Scrape("10086"))
 }
