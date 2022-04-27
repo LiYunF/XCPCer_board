91326fdbd5d4ec7fa669175eead1bf9113bf3f78
AcidMangos <1124497942@qq.com>
2022-04-27 18:41:42 +0800

    Update atcoder_test.go

--- spider/atcoder/atcoder_test.go
+++ spider/atcoder/atcoder_test.go
@@ -5,63 +5,26 @@ import (
 	"testing"
 )
 
-//检查函数，若不一致返回1
-func isIntMsgDifferent(funcRet, ans map[string]int) bool {
-	for k, v := range ans {
-		if _, r := funcRet[k]; r == false || v != funcRet[k] {
+var atcTestData = map[string]int{
+	"atc_contest_sum": 6,
+	"atc_rating":      785,
+}
+
+func dataCheck(atcData map[string]int) bool {
+	for key, val := range atcTestData {
+		if _, data := atcData[key]; data == false || atcData[key] != val {
 			return true
 		}
 	}
 	return false
 }
-func isStringMsgDifferent(funcRet, ans map[string]string) bool {
-	for k, v := range ans {
-		if _, r := funcRet[k]; r == false || v != funcRet[k] {
-			return true
-		}
+
+func TestAtc(t *testing.T) {
+	atcRes, err := ScrapeAll(model.TestAtcIdLQY)
+	if err != nil {
+		t.Errorf("Error of atcoder is : %v", err)
 	}
-	return false
-}
-
-//判断并输出错误
-func checkIntError(t *testing.T, uid string, tp string, all func(uid string) (map[string]int, error),
-	acInt map[string]int) {
-	if ret, err := all(uid); isIntMsgDifferent(ret, acInt) {
-		if err != nil {
-			t.Errorf("Error of %v in all msg: %v", tp, err)
-		}
-		t.Errorf("Error of %v in all msg\n ret= %v  \nbut the ans is %v", tp, ret, acInt)
+	if dataCheck(atcRes) {
+		t.Errorf("Error of atcoder \n ans is %v \n but output is %v", atcTestData, atcRes)
 	}
-}
-func checkStrError(t *testing.T, uid string, tp string, all func(uid string) (map[string]string, error),
-	acInt map[string]string) {
-	if ret, err := all(uid); isStringMsgDifferent(ret, acInt) {
-		if err != nil {
-			t.Errorf("Error of %v in all msg: %v", tp, err)
-		}
-		t.Errorf("Error of %v in str msg\n ret= %v  \nbut the ans is %v", tp, ret, acInt)
-	}
-}
-
-func atCoderTest(t *testing.T) {
-
-	tp := "atCoder"
-	fc1 := ScrapeAll
-	var uid string
-	var atcInt map[string]int
-
-	//输入个例
-	uid = model.TestLuoGuIdLYF
-	atcInt = map[string]int{
-		"atc_contest_sum": 6,
-		"atc_rating":      785,
-	}
-
-	//开始测试
-	checkIntError(t, uid, tp, fc1, atcInt)
-
-}
-
-func TestLg(t *testing.T) {
-	atCoderTest(t)
 }
