026b4bad898546d13692e08e14a950007654dd11
AcidMangos <1124497942@qq.com>
2022-05-02 22:24:54 +0800

    update atcoder
    增加总过题数

---
+++ spider/atcoder/contestlist.go
@@ -0,0 +1,61 @@
+package atcoder
+
+import (
+	"XCPCer_board/scraper"
+	"github.com/gocolly/colly"
+	"strconv"
+)
+
+const (
+	//key
+	contestKey = "atc_contest_id"
+
+	//keyword
+
+)
+
+var (
+	contestScraper *scraper.Scraper[string]
+)
+
+// 初始化
+func init() {
+	contestScraper = scraper.NewScraper[string](
+		scraper.WithCallback(contestCallback),
+		scraper.WithThreads[string](2),
+	)
+}
+
+//处理 contestHistory 的页面回调
+func contestCallback(c *colly.Collector, res *scraper.Results[string]) {
+	//用goquery
+	num := 1
+
+	c.OnHTML("td[class=\"text-left\"]", func(element *colly.HTMLElement) {
+		str := strconv.Itoa(num)
+		res.Set(contestKey+"_"+str, getAtCoderContestId(element))
+		num = num + 1
+	})
+}
+
+//获取 userID
+func getAtCoderHistoryUrl(atCoderId string) string {
+	return "https://atcoder.jp/users/" + atCoderId + "/history"
+}
+
+//获取 contestId
+func getAtCoderContestId(e *colly.HTMLElement) string {
+	link := e.ChildAttr("a:first-child", "href")
+	//fmt.Println(link)
+	return link
+}
+
+//-------------------------------------------------------------------------------------------//
+// 对外暴露函数
+//-------------------------------------------------------------------------------------------//
+
+//FetchMainPage 抓取个人主页页面所有
+
+func FetchContestHistory(uid string) scraper.Results[string] {
+	return contestScraper.Scrape(getAtCoderHistoryUrl(uid))
+}
