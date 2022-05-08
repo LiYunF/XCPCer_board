cf2b4c9e42ae2ffec9ef34fb9ff4693c19c619e6
sariel <2986325137@qq.com>
2022-05-08 11:55:27 +0800

    111

--- db/mysql/init.go
+++ db/mysql/init.go
@@ -13,16 +13,13 @@ var Db *sql.DB
 //初始化数据库的函数
 func init() {
 	msg := config.Config.Database.MysqlConf
-
 	password := msg.Password
 	host := msg.Host
 	dbname := msg.Name
 	user := msg.Username
 	dsn := fmt.Sprintf("%v:%v@tcp(%v)/%v", user, password, host, dbname)
-
 	var err error
 	Db, err = sql.Open("mysql", dsn)
-
 	if err != nil {
 		log.Errorf("Open Sql Error: %v", err)
 
@@ -30,5 +27,4 @@ func init() {
 	//fmt.Println("成功打开MYSQL")
 	Db.SetMaxOpenConns(20)
 	Db.SetMaxIdleConns(10)
-
 }
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"XCPCer_board/config"
 	_ "XCPCer_board/db/Redis"
 	_ "XCPCer_board/db/mysql"
-
 	"fmt"
 	_ "github.com/FengZhg/go_tools/gin_logrus"
 )
@@ -12,9 +11,7 @@ import (
 // 主入口函数
 func main() {
 
-
 	fmt.Println(config.Config.Database.RedisConf.Host)
 	//fmt.Println(luogu.ScrapeAll(model.TestLuoGuIdLYF))
-
 
 }
