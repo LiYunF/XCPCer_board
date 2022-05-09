c16e137c82ef75bf278c38f6918c04f86461a207
AcidMangos <1124497942@qq.com>
2022-05-09 20:16:58 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,13 +3,9 @@ package main
 import (
 	_ "XCPCer_board/config"
 	_ "XCPCer_board/dao"
-	"XCPCer_board/model"
-	"XCPCer_board/spider/atcoder"
-	"fmt"
 	_ "github.com/FengZhg/go_tools/gin_logrus"
 )
 
 // 主入口函数
 func main() {
-	fmt.Println(atcoder.ScrapeSubmission(model.TestAtcIdLQY))
 }
