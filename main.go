57911fad6de32bf8c02564b4c6885233f0281bd0
AcidMangos <1124497942@qq.com>
2022-05-12 20:51:47 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,29 +2,11 @@ package main
 
 import (
 	_ "XCPCer_board/config"
-	"XCPCer_board/dao"
 	_ "XCPCer_board/dao"
-	"XCPCer_board/model"
-	"XCPCer_board/spider/vjudge"
-	"context"
-	"fmt"
 	_ "github.com/FengZhg/go_tools/gin_logrus"
 )
 
 // 主入口函数
 func main() {
-	
-}
 
-func init() {
-	redisClient, err := dao.NewRedisClient()
-	if err != nil {
-		panic(err)
-	}
-	dbClient, err := dao.NewDBClient()
-	if err != nil {
-		panic(err)
-	}
-	dao.RedisClient = redisClient
-	dao.DBClient = dbClient
 }
